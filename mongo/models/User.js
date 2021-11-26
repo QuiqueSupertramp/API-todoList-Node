00bcc1c781fa11cf2a188f69af3dba16e07c32d7
Quique Supertramp <quiquesupertramp@gmail.com>
2021-11-26 09:15:41 +0100

    usuarios añadidos

--- mongo/models/User.js
+++ mongo/models/User.js
@@ -1,33 +1,36 @@
 const { model, Schema } = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: [true, "EMAIL REQUIRED"],
-    unique: true,
-  },
-  name: {
-    type: String,
-    required: [true, "NAME REQUIRED"],
-  },
-  password: {
-    type: String,
-    required: [true, "PASSWORD REQUIRED"],
-  },
-  folders: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "folders",
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "EMAIL REQUIRED"],
+      unique: true,
     },
-  ],
-  tasks: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "tasks",
+    name: {
+      type: String,
+      required: [true, "NAME REQUIRED"],
     },
-  ],
-},  { versionKey: false });
+    password: {
+      type: String,
+      required: [true, "PASSWORD REQUIRED"],
+    },
+    folders: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "folders",
+      },
+    ],
+    tasks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "tasks",
+      },
+    ],
+  },
+  { versionKey: false }
+);
 
 userSchema.methods.encryptPassword = async (password) => {
   const salt = await bcrypt.genSalt(10);
